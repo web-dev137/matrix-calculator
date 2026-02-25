2a66c14a6f25b5c9265340d33c04165263ced467
web-dev137 <daniyar.tuletsov.1994@mail.ru>
2026-02-26 00:28:26 +0700

    fix main

--- main.cpp
+++ main.cpp
@@ -1,94 +1,74 @@
 #include "Matrix.hpp"
 #include <functional>
+#include <unordered_map>
 #include <windows.h>
+#include <iostream>
 
-/**
- * 
- */
 template <typename T>
 using Operation = std::function<void(Matrix<T>&)>;
 
-/**
- * Ассоциативный массив команд, для реализации
- * паттерна Command
- */
-std::unordered_map<int, Operation<int>> ops;
-
-/**
- * Метод добавления операций/комманд
- */
-template <typename T>
-void addOperation(int n,Operation<T> op, std::unordered_map<int, Operation<T>>& ops) {
-    ops[n] = op;
-}
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-    int rows,cols;
-    Matrix<int> A,B,C;
-    int choice = 1,number;
 
-    while(true) {
-        std::cout<<"1)Умножение матриц A*B"<<std::endl;
-        std::cout<<"2)Умножение матрицы на число A*number"<<std::endl;
-        std::cout<<"3)Транспонирование матрицы:"<<std::endl;
-        std::cout<<"0)Выход:"<<std::endl;
+    std::unordered_map<int, Operation<int>> ops;
 
-        std::cin>>choice;
+    // Регистрация команд 
+    ops[1] = [](Matrix<int>& A) {
+        int rows, cols;
+        std::cout << "Введите размерность матрицы B: ";
+        std::cin >> rows >> cols;
 
-        if(choice == 0) {
-            std::cout<<"Выход"<<std::endl;
-            return 0;
+        Matrix<int> B(rows, cols);
+        std::cout << "Введите данные в матрицу B: ";
+        std::cin >> B;
+
+        Matrix<int> C = A * B;
+
+        std::cout << "Результат:\n" << C;
+    };
+
+    ops[2] = [](Matrix<int>& A) {
+        int number;
+        std::cout << "Введите число: ";
+        std::cin >> number;
+
+        Matrix<int> C = A * number;
+        std::cout << "Результат:\n" << C;
+    };
+
+    ops[3] = [](Matrix<int>& A) {
+        Matrix<int> C = !A;
+        std::cout << "Результат:\n" << C;
+    };
+
+    while (true) {
+        std::cout << "\n1) Умножение A * B\n";
+        std::cout << "2) Умножение A * number\n";
+        std::cout << "3) Транспонирование\n";
+        std::cout << "0) Выход\n";
+
+        std::cout<<"Выберите пункт в меню:"<<std::endl;
+        int choice;
+        std::cin >> choice;
+
+        if (choice == 0) break;
+
+        if (ops.find(choice) == ops.end()) {
+            std::cout << "Неверный выбор\n";
+            continue;
         }
-        std::cout<<"Введите размерность матрицы A <количество строк> <количество столбцов>:";
-        std::cin>>rows>>cols;
-        Matrix<int>A(rows,cols);
-        std::cout<<"Введите данные в матрицу:";
-        std::cin>>A;
-        Matrix<int> C(rows,cols);
 
-        /**
-         * Операция умножения  матриц
-         */
-        Operation<int> multiplyMatrixes = [&B,&C,&rows,&cols](Matrix<int>& A) {
-            std::cout<<"Введите размерность матрицы B <количество строк> <количество столбцов>:";
-            std::cin>>rows>>cols;
-            Matrix<int>B(rows,cols);
-            std::cout<<"Введите данные в матрицу:";
-            std::cin>>B;
-            C=A*B;
-            std::cout<<"Результат:"<<std::endl;
-            std::cout<<C;
-        };
+        int rows, cols;
+        std::cout << "Введите размерность матрицы A: ";
+        std::cin >> rows >> cols;
 
-        /**
-         * Операция умножения матрицы на число
-         */
-        Operation<int> multiplyMatrixOnNumber = [&number,&C](Matrix<int>& A) {
-            std::cout<<"Введите число:"<<std::endl;
-            std::cin>>number;
-            C = A*number;
-            std::cout<<"Результат:"<<std::endl;
-            std::cout<<C;
-        };
+        Matrix<int> A(rows, cols);
+        std::cout << "Введите данные в матрицу A: ";
+        std::cin >> A;
 
-        /**
-         * Транпонирование матрицы
-         */
-        Operation<int> transponseMatrix = [&C](Matrix<int>& A) {
-            C = !A;
-            std::cout<<"Результат:"<<std::endl;
-            std::cout<<C;
-        };
-
-       
-        /**
-         * Регистрация операций
-         */
-        addOperation(1,multiplyMatrixes,ops);
-        addOperation(2,multiplyMatrixOnNumber,ops);
-        addOperation(3,transponseMatrix,ops);
-        Operation<int> f = ops[choice]; //выбор операции
-        f(A);//вызов метода
+        ops[choice](A);
     }
+
+    return 0;
 }
